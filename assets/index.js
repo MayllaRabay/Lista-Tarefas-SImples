f76566dc417feb7432876fef407e7a2e71298fe9
Maylla Rabay <mayllarabay@gmail.com>
2021-04-04 19:01:31 -0300

    Função de checkbox adicionada ao JS

--- assets/index.js
+++ assets/index.js
@@ -2,13 +2,24 @@
 const form = document.querySelector("form");
 const input = form.querySelector("input");
 const aFazer = document.querySelector(".a-fazer");
-const feitas = document.querySelector("feitas");
+const feitas = document.querySelector(".feitas");
 
 function deletarTarefa(event) {
     const deleteButton = event.target;
     const tarefa = deleteButton.closest("li");
 
     tarefa.remove();
+}
+
+function marcarComoFeita(event) {
+    const checkbox = event.target;
+    const tarefa = checkbox.closest("li");
+
+    if(checkbox.checked) {
+        feitas.append(tarefa);
+    } else {
+        aFazer.append(tarefa);
+    };
 }
 
 form.addEventListener("submit", event => {
@@ -18,13 +29,13 @@ form.addEventListener("submit", event => {
 
     const checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
+    checkbox.addEventListener("input", marcarComoFeita);
 
     const text = document.createElement("span");
     text.innerText = input.value;
 
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "Deletar";
-
     deleteButton.addEventListener("click", deletarTarefa);
 
     tarefa.append(checkbox);
